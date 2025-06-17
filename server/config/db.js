7e13e2373dde4b1f8f7cb9c02750d386d5c7d22d
Szymon <szymers12345@gmail.com>
2025-06-17 16:55:17 +0200

    change cors

--- server/config/db.js
+++ server/config/db.js
@@ -1,28 +1,25 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-// Tworzenie puli połączeń
 const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
-  connectionLimit: 10, // Maksymalna liczba połączeń
+  connectionLimit: 10,
   queueLimit: 0,
 });
 
-// Sprawdzanie połączenia
 db.getConnection((err, connection) => {
   if (err) {
     console.error("❌ Błąd połączenia z bazą danych:", err.message);
     return;
   }
   console.log("✅ Połączono z bazą danych MySQL");
-  connection.release(); // Zwolnienie połączenia do puli
+  connection.release();
 });
 
-// Pingowanie bazy co 60 sekund, aby uniknąć zamknięcia połączenia
 setInterval(() => {
   db.getConnection((err, connection) => {
     if (err) {
@@ -33,7 +30,6 @@ setInterval(() => {
       connection.release();
     }
   });
-}, 60000); // Co 60 sekund
+}, 60000);
 
-// Eksportowanie `db`
 module.exports = db;
